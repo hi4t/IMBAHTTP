71562e92d22834bacd8f81bc194908e4f7be8e89
zace <zace.cn@gmail.com>
2016-04-28 11:51:21 +0800

    download

--- app/src/main/java/com/imba/imbahttp/AbstractCallBack.java
+++ app/src/main/java/com/imba/imbahttp/AbstractCallBack.java
@@ -9,45 +9,62 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 
 /**
- * Created by zace on 2016/4/26.
+ * Created by zace on 2015/4/26.
  */
 public abstract class AbstractCallBack<T> implements ICallBack<T> {
 
     private String path;
 
     @Override
-    public T parsr(HttpURLConnection conn) throws Exception {
+    public T parsr(HttpURLConnection conn, OnProgressUpdateListener listener) throws Exception {
         int status = conn.getResponseCode();
         if (status == HttpURLConnection.HTTP_OK) {
-            OutputStream out = null;
 
             if (!TextUtils.isEmpty(path)) {
-                out = new FileOutputStream(path);
-            } else {
-                out = new ByteArrayOutputStream();
-            }
+                FileOutputStream out = new FileOutputStream(path);
+                InputStream is = conn.getInputStream();
+                byte[] buffer = new byte[2048];
 
-            InputStream is = conn.getInputStream();
-            byte[] buffer = new byte[2048];
+                int len;
+                int currentLen = 0;
+                int totalLen = conn.getContentLength();
 
-            int len;
+                while ((len = is.read(buffer)) != -1) {
+                    out.write(buffer, 0, len);
+                    currentLen += len;
+                    if (listener != null) {
+                        listener.onProgressUpdaet(currentLen, totalLen);
+                    }
 
-            while ((len = is.read(buffer)) != -1) {
-                out.write(buffer, 0, len);
-            }
-            is.close();
-            out.flush();
-            out.close();
-
-            if (!TextUtils.isEmpty(path)) {
+                }
+                is.close();
+                out.flush();
+                out.close();
                 bindData(path);
+
             } else {
-                String result = new String(((ByteArrayOutputStream) out).toByteArray());
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                InputStream is = conn.getInputStream();
+                byte[] buffer = new byte[2048];
+
+                int len;
+
+                while ((len = is.read(buffer)) != -1) {
+                    out.write(buffer, 0, len);
+                }
+                is.close();
+                out.flush();
+                out.close();
+                String result = new String(out.toByteArray());
                 bindData(result);
             }
         }
 
         return null;
+    }
+
+    @Override
+    public void onProgressUpdate(long currentLen, long totalLen) {
     }
 
     public abstract T bindData(String result);
